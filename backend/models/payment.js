ef74d9572c0da419ae2e905f03f833e804094040
hemanth <hemmon963@gmail.com>
2025-05-31 11:13:40 +0530

    Payement schema updated

---
+++ backend/models/payment.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const paymentSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    booking_id:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Booking",
+        required: true,
+    },
+    payment_method: {
+        type: String,
+        enum: ['razorpay', 'credit_card', 'debit_card', 'upi', 'netbanking'],
+        required: true,
+    },
+    transaction_id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+      },
+      payment_status: {
+        type: String,
+        enum: ['success', 'failed', 'pending'],
+        default: 'pending',
+      },
+      payment_date: {
+        type: Date,
+        default: Date.now,
+      },
+}, { timestamps: true });
+const Payment = mongoose.model('Payment', paymentSchema);
+export default Payment;
