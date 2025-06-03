ee735f81ab49eac7f92c1b7fe708073d302074d8
hemanth <hemmon963@gmail.com>
2025-06-03 14:02:03 +0530

    payement routes updated

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, verifyPayment } from '../controllers/paymentController.js';
+import { createOrder, verifyPayment, getCompletedPayments, getFailedPayments, getPendingPayments } from '../controllers/paymentController.js';
 import { verifyUser } from '../middlewares/authUser.js';
 
 const router = express.Router();
@@ -8,5 +8,10 @@ const router = express.Router();
 router.post('/create-order', verifyUser, createOrder);
 //Route to verify payment signature
 router.post('/verify-payment', verifyUser, verifyPayment);
+
+router.get('/payments/pending', verifyUser, getPendingPayments);
+router.get('/payments/completed', verifyUser, getCompletedPayments);
+router.get('/payments/failed', verifyUser, getFailedPayments);
+
 
 export default router;
